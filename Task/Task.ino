3d218e999af6f172140608c0ef31e91393e67e3c
DarkNebulaaa <mason44211@gmail.com>
2022-10-07 14:53:43 +0800

    v3

--- Task/Task.ino
+++ Task/Task.ino
@@ -58,9 +58,9 @@ void Task2code( void * pvParameters ){
 
   for(;;){
     digitalWrite(led2, HIGH);
-    delay(300);
+    delay(100);
     digitalWrite(led2, LOW);
-    delay(300);
+    delay(100);
   }
 }
 
